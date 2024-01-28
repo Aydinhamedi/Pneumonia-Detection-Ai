900038f77c0349e7e4f92f75afc719318b149969
Aydin <108932477+Aydinhamedi@users.noreply.github.com>
2024-01-28 21:24:34 +0330

    deleted:    Interface/GUI/Data/GUI_layout.py 	modified:   Interface/GUI/Data/GUI_main.py

--- Interface/GUI/Data/GUI_main.py
+++ Interface/GUI/Data/GUI_main.py
@@ -43,8 +43,6 @@ from Utils.Grad_cam import make_gradcam_heatmap
 from Utils.print_color_V2_NEW import print_Color_V2
 from Utils.print_color_V1_OLD import print_Color
 from Utils.Other import *
-# GUI
-from GUI_layout import *
 # global vars>>>
 # CONST SYS
 GUI_Ver = '0.8.9.7'
@@ -99,6 +97,48 @@ tf.get_logger().setLevel('ERROR')
 physical_devices = tf.config.list_physical_devices('GPU')
 for gpu_instance in physical_devices:
     tf.config.experimental.set_memory_growth(gpu_instance, True)
+# Making the GUI layout >>>
+# prep GUI
+sg.theme('GrayGrayGray') 
+# Main
+GUI_layout_Tab_main = [
+    [sg.Text('Enter the image dir:', font=(None, 10, "bold"))],
+    [
+        sg.Input(key='-INPUT_IMG_dir-'),
+        sg.Button('Browse', key='-BUTTON_BROWSE_IMG_dir-'),
+        sg.Button('Ok', key='-BUTTON_OK_IMG_dir-')
+    ],
+    [sg.Text('Log:', font=(None, 10, "bold"))],
+    [sg.Multiline(key='-OUTPUT_ST-', size=(54, 6), autoscroll=True)],
+    [sg.Text('Result:', font=(None, 10, "bold"))],
+    [sg.Text(key='-OUTPUT_ST_R-', size=(50, 2), background_color='white')],
+    [
+        sg.Checkbox('Show Grad-CAM', key='-CHECKBOX_SHOW_Grad-CAM-', default=True),
+        sg.Checkbox('Show DICOM Info', key='-CHECKBOX_SHOW_DICOM_INFO-', default=True)
+    ],
+    [
+        sg.Button('Analyse'),
+        sg.Button('Close')
+    ]
+]
+# Other
+GUI_layout_Tab_other = [
+    [sg.Text('Ai Model Settings:', font=(None, 10, "bold"))],
+    [
+        sg.Button('Update/Download Model', key='-BUTTON_UPDATE_MODEL-'),
+        sg.Button('Reload Model', key='-BUTTON_RELOAD_MODEL-')
+    ],
+    [
+        sg.Checkbox('Download Light Model', key='-CHECKBOX_DOWNLOAD_LIGHT_MODEL-', default=False)
+    ],
+    [sg.Text('Ai Model Info:', font=(None, 10, "bold"))],
+    [
+        sg.Text(key='-OUTPUT_Model_info-', size=(40, 7), pad=(4, 0))
+    ]
+]
+# DICOM Info
+def C_GUI_layout_DICOM_Info_Window():
+    return [[sg.Multiline(key='-OUTPUT_DICOM_Info-', size=(100, 42), autoscroll=True)]]
 # crator
 CSAA = '''
 ~*
@@ -351,7 +391,7 @@ def CI_liid(img_dir, Show_DICOM_INFO: bool = True) -> str:
                 if Show_DICOM_INFO:
                     GUI_layout_DICOM_Info_Window_copy = C_GUI_layout_DICOM_Info_Window()
                     GUI_window = sg.Window('DICOM Info', GUI_layout_DICOM_Info_Window_copy)
-                    GUI_window.read(timeout=1, timeout_key='-TIMEOUT-')
+                    GUI_window.finalize()
                     # Write DICOM info to the window
                     for element in ds:
                         if element.name != "Pixel Data":
@@ -469,8 +509,6 @@ def main():
     # prep var
     IMG_dir = None
     Update_model_info_LXT = None
-    # prep GUI
-    sg.theme('GrayGrayGray') 
     # Create the tabs
     GUI_tab_main = sg.Tab('Main', GUI_layout_Tab_main)
     GUI_tab_other = sg.Tab('Ai Model', GUI_layout_Tab_other)
