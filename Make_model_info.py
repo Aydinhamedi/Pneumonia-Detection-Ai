39a5afa41b72d41e0944008a8ebc98dce6f1eb71
Aydin <108932477+Aydinhamedi@users.noreply.github.com>
2024-02-10 22:14:49 +0330

    modified:   BETA_E_Model_T&T.ipynb 	modified:   Make_model_info.py 	modified:   Model_T&T.ipynb

--- Make_model_info.py
+++ Make_model_info.py
@@ -2,19 +2,44 @@ import os
 import hashlib
 import json
 
+
 def calculate_hash(file_path):
+    """Calculates a SHA256 hash for the contents of the file at the given path.
+
+    Args:
+        file_path: The path to the file to hash.
+
+    Returns: 
+        The hex string of the SHA256 hash.
+    """
     with open(file_path, 'rb') as f:
         bytes = f.read()
         readable_hash = hashlib.sha256(bytes).hexdigest()
     return readable_hash
 
+
 def check_file_type(file_name):
+    """Checks if a file name contains 'weight' to determine the file type.
+
+    Args:
+        file_name (str): The file name to check.
+
+    Returns:
+        str: 'Weight' if 'weight' is in the name, 'Full' otherwise.
+    """
     if 'weight' in file_name.lower():
         return 'Weight'
     else:
         return 'Full'
 
 def main():
+    """
+    Generates a JSON file containing hashes and metadata for all model files in the 'models/Ready' folder. 
+
+    Iterates through all subfolders in 'models/Ready', gets file info for each model file, 
+    and saves it to a dict where the key is the file hash. Writes this model info dict 
+    to 'model_info.json'.
+    """
     base_folder_path = 'models\Ready'
     model_info = {}
     for dir_name in os.listdir(base_folder_path):
@@ -25,14 +50,14 @@ def main():
                 file_hash = calculate_hash(file_path)
                 file_type = check_file_type(file_name)
                 data = {
-                    'name': file_name,
-                    'Ver': dir_name,  
+                    'Ver': dir_name,
                     'stored_type': file_type
                 }
                 model_info[file_hash] = data
 
     with open('model_info.json', 'w') as json_file:
         json.dump(model_info, json_file)
+
 
 if __name__ == "__main__":
     main()
