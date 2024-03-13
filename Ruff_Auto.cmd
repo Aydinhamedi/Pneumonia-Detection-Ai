740a27f5fa95c1b96111137e2f818d87be306fdd
Aydin <108932477+Aydinhamedi@users.noreply.github.com>
2024-03-13 14:04:24 +0330

    modified:   .gitignore 	modified:   Ruff_Auto.cmd 	modified:   requirements.txt

--- Ruff_Auto.cmd
+++ Ruff_Auto.cmd
@@ -1,4 +1,6 @@
-echo Running automated checks and formatting...
-ruff check
-ruff format .
-ruff clean
+@echo off
+echo (Batch script) Running automated checks and formatting...
+ruff check . > ruff_check_results.txt
+ruff format . > ruff_format_results.txt
+ruff clean >nul 2>&1
+echo (Batch script) Done.
