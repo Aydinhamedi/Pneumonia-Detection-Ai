7c59c892998ac70e1bded44a516e7255c6fbea87
Aydin <108932477+Aydinhamedi@users.noreply.github.com>
2024-03-23 17:23:39 +0330

    modified:   .gitignore 	modified:   BETA_E_Model_T&T.ipynb 	modified:   Utils/Other.py 	modified:   Utils/README.md

--- Utils/Other.py
+++ Utils/Other.py
@@ -1,6 +1,7 @@
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 from Utils.print_color_V2_NEW import print_Color_V2
 from Utils.print_color_V1_OLD import print_Color
+import keras.backend as K
 from tabulate import tabulate
 from numba import cuda
 import numpy as np
@@ -128,3 +129,37 @@ def evaluate_model_full(y_test, model_pred, model=None, x_test=None):
 
     # Print the table
     print(tabulate(metrics, headers=["Metric", "Value"], tablefmt="pretty"))
+
+
+def set_optimizer_attribute(optimizer, attribute, value, verbose: bool = False):
+    """Sets an attribute on the given optimizer to the specified value.
+
+    Args:
+        optimizer: The optimizer instance to modify.
+        attribute: The attribute name to set.
+        value: The value to set the attribute to.
+        verbose: Whether to print a message if the attribute does not exist.
+    """
+    if hasattr(optimizer, attribute):
+        K.set_value(getattr(optimizer, attribute), value)
+    else:
+        print(f"The optimizer does not have an attribute named '{attribute}'") if verbose else None
+
+
+def print_optimizer_info(model):
+    """Prints information about the optimizer used by a Keras model.
+
+    Prints the optimizer class name and its parameter values. Useful
+    for inspecting optimizer configuration.
+
+    Args:
+        model: The Keras model whose optimizer to print info for.
+
+    """
+    if model.optimizer:
+        print_Color(f"~*Optimizer: ~*{model.optimizer.__class__.__name__}", ["cyan", "green"], advanced_mode=True)
+        print_Color(" <Opt> Parameters:", ["cyan"])
+        for param, value in model.optimizer.get_config().items():
+            print_Color(f"~* <Opt> -- ~*{param}: ~*{value}", ["cyan", "light_cyan", "green"], advanced_mode=True)
+    else:
+        print_Color("No optimizer found in the model.", ["red"])
